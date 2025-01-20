b0523fa9f4a9c10426e0bc9eeab2a87b357c8ecf
harshavardhan.r <harshavardhan.r@GSPSFJ9S542>
2025-01-20 11:49:13 +0530

    Add project files.

---
+++ LibraryManagement.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
